f4f0fa3a7af5f0761add667efeecadd79f6a1901
Ryan Spangler <spanglry@stanford.edu>
2018-08-21 11:33:58 -0700

    adding simple logic evaluator, fully convert network to json

--- src/Reactions.jl
+++ src/Reactions.jl
@@ -2,7 +2,7 @@ module Reactions
 
 import JSON
 
-function readreactions(path)
+function readtsv(path)
     reactions = Dict{String, Dict{String, Float64}}()
     open(path) do file
         for line in enumerate(eachline(file))
@@ -15,6 +15,50 @@ function readreactions(path)
         end
     end
     reactions
+end
+
+function readjson(path)
+    JSON.parsefile(path)
+end
+
+function moleculestate(network)
+    molecules = Dict{String, Float64}()
+
+    for molecule in keys(network["initial"])
+        molecules[molecule] = network["initial"][molecule]
+    end
+
+    for reaction in keys(network["reactions"])
+        for molecule in keys(network["reactions"][reaction]["reaction"])
+            if !(molecule in keys(molecules))
+                molecules[molecule] = 0
+            end
+        end
+    end
+
+    molecules
+end
+
+logicoperations = Dict(
+    "if" => x -> x[1] > 0,
+    "not" => x -> x[1] == 0,
+    ">" => x -> x[1] > x[2],
+    "and" => all,
+    "or" => any
+)
+
+function runlogic(logic, state, ops)
+    if typeof(logic) == String
+        if logic[1] == '"'
+            logic[2:end-1]
+        else
+            state[logic]
+        end
+    else
+        op = ops[logic[1]]
+        args = map(sublogic -> runlogic(sublogic, state, ops), logic[2:end])
+        op(args)
+    end
 end
 
 end
