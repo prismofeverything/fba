d4726ea3f954e5980c33bde8c85f25b1abbb85d9
Ryan Spangler <spanglry@stanford.edu>
2018-08-21 15:35:03 -0700

    initializing stoichiometric matrix based on network values and regulation

--- src/Reactions.jl
+++ src/Reactions.jl
@@ -21,6 +21,14 @@ function readjson(path)
     JSON.parsefile(path)
 end
 
+logicoperations = Dict(
+    "is" => x -> isa(x[1], Bool) ? x[1] : x[1] > 0,
+    "not" => x -> isa(x[1], Bool) ? !x[1] : x[1] == 0,
+    ">" => x -> x[1] > x[2],
+    "and" => all,
+    "or" => any
+)
+
 function moleculestate(network)
     molecules = Dict{String, Float64}()
 
@@ -39,26 +47,53 @@ function moleculestate(network)
     molecules
 end
 
-logicoperations = Dict(
-    "if" => x -> x[1] > 0,
-    "not" => x -> x[1] == 0,
-    ">" => x -> x[1] > x[2],
-    "and" => all,
-    "or" => any
-)
-
 function runlogic(logic, state, ops)
-    if typeof(logic) == String
-        if logic[1] == '"'
-            logic[2:end-1]
-        else
-            state[logic]
-        end
-    else
+    if isa(logic, Number)
+        logic
+    elseif isa(logic, String)
+        logic[1] == '"' ? logic[2:end-1] : get(state, logic, 0)
+    elseif !isempty(logic)
         op = ops[logic[1]]
         args = map(sublogic -> runlogic(sublogic, state, ops), logic[2:end])
         op(args)
+    else
+        true
     end
+end
+
+function applylogic(logicmap, state, ops)
+    Dict(key => runlogic(logic, state, ops) for (key, logic) in logicmap)
+end
+
+function buildcolumn(molecules, reaction)
+    map(m -> m in keys(reaction) ? reaction[m] : 0, molecules)
+end
+
+function buildrow(molecules, regulation, reaction, ops)
+    allowed = runlogic(reaction["regulation"], regulation, ops)
+    if allowed
+        buildcolumn(molecules, reaction["reaction"])
+    else
+        zeros(Float64, length(molecules))
+    end
+end
+
+function buildmatrix(molecules, reactions, network, state, ops)
+    regulation = applylogic(network["regulation"], state, ops)
+    hcat(map(r -> buildrow(molecules, regulation, network["reactions"][r], ops), reactions)...)
+end
+
+function initialize(network, ops)
+    state = moleculestate(network)
+    molecules = sort(collect(keys(state)))
+    reactions = sort(collect(keys(network["reactions"])))
+    stoichiometry = buildmatrix(molecules, reactions, network, state, ops)
+
+    Dict(
+        "molecules" => molecules,
+        "reactions" => reactions,
+        "stoichiometry" => stoichiometry
+    )
 end
 
 end
